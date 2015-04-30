c0b0ed8fddab261ca4f622ab86cbc089cff5c7bb
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-30 19:40:13 +0200

    Add a docblock

--- module/index.js
+++ module/index.js
@@ -1,3 +1,18 @@
+ /**
+  * Transposes the given two-dimensional *array* as if it were a matrix.
+  *
+  * Returns a new array, flipped against the diagonal of the matrix.
+  *
+  * @param  {Array[]}  array
+  *   A two-dimensional array
+  *
+  * @returns  {Array[]}
+  *   A copy of *array*, transposed.
+  *
+  * @alias     flip
+  * @module    flip-array
+  * @function  default
+  */
 export default (array) => {
   if (!Array.isArray(array)) throw new TypeError(
     'flip-array: Make sure you pass an `{Array[]} array`.'
